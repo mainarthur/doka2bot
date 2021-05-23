c3b7a8f4c25895cee9608c4fd347bd137e0b17fc
Arthur Kh <listbox6@gmail.com>
2021-05-23 19:39:16 +0300

    pagination for inline queries

--- src/queues/proccessors/inlineQueryJobProcessor.js
+++ src/queues/proccessors/inlineQueryJobProcessor.js
@@ -1,6 +1,10 @@
 const bot = require('../../bots/bot')
 const { MAX_INLINE_RESULTS } = require('../../constants')
+const logger = require('../../logger')
+const Timedelta = require('../../types/Timedelta')
 const inlineQueryExecuter = require('./executers/inlineQueryExecuter')
+
+const getNowSec = () => Math.floor(Date.now() / 1000)
 
 /**
  *
@@ -10,15 +14,40 @@ const inlineQueryJobProcessor = async ({ data: inlineQuery }) => {
   const {
     query,
     id: queryId,
-    from: { language_code: locale },
+    from: { id: userId, language_code: locale },
+    offset,
   } = inlineQuery
+  const currentOffset = +(offset || 0)
+  const start = getNowSec()
 
-  const results = inlineQueryExecuter({ query, locale })
+  try {
+    console.log(offset, currentOffset)
+    const results = inlineQueryExecuter({ query, locale })
+    console.log({ offset })
+    const answered = await bot.answerInlineQuery(
+      queryId,
+      results.slice(
+        MAX_INLINE_RESULTS * currentOffset,
+        MAX_INLINE_RESULTS * (currentOffset + 1),
+      ),
+      {
+        cache_time: 0,
+        is_personal: true,
+        next_offset: `${currentOffset + 1}`,
+      },
+    )
 
-  await bot.answerInlineQuery(queryId, results.slice(0, MAX_INLINE_RESULTS), {
-    cache_time: 0,
-    is_personal: true,
-  })
+    if (answered) {
+      logger.log(
+        `[${new Date().toLocaleString()}][#id${userId}][inlineQuery] ${query} ${new Timedelta(
+          start,
+          getNowSec(),
+        )}`,
+      )
+    }
+  } catch (err) {
+    logger.err(err)
+  }
 }
 
 module.exports = inlineQueryJobProcessor
