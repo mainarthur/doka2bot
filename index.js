f7221b6e55680cc572503c51566e0ed68f422a07
Arthur Kh <listbox6@gmail.com>
2021-05-24 16:50:48 +0300

    new files location

--- index.js
+++ index.js
@@ -9,22 +9,24 @@ const SearchEngineRepository = require('./src/api/SearchEngineRepository')
 const { ALLOWED_UPDATES } = require('./config.json')
 
 void (async () => {
-  await DocumentationRepository.load(
-    path.join(__dirname, './src/documentations'),
-  )
-  await SearchEngineRepository.load(path.join(__dirname, './src/search'))
-  for (const updateType of ALLOWED_UPDATES) {
-    // @ts-ignore
-    bot.on(updateType, updateHandler(updateType))
-  }
+  try {
+    await DocumentationRepository.load(path.join(__dirname, './documentations'))
+    await SearchEngineRepository.load(path.join(__dirname, './search'))
+    for (const updateType of ALLOWED_UPDATES) {
+      // @ts-ignore
+      bot.on(updateType, updateHandler(updateType))
+    }
 
-  await bot.startPolling({
-    polling: {
-      params: {
-        allowed_updates: ALLOWED_UPDATES,
+    await bot.startPolling({
+      polling: {
+        params: {
+          allowed_updates: ALLOWED_UPDATES,
+        },
       },
-    },
-  })
+    })
 
-  logger.log(`Bot is started at ${new Date().toLocaleString()}`)
+    logger.log(`Bot is started at ${new Date().toLocaleString()}`)
+  } catch (err) {
+    logger.err(err)
+  }
 })()
