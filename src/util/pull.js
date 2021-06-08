37d91857dfd127616ca3bdb92e274100a4c8c6cc
Arthur Kh <listbox6@gmail.com>
2021-06-08 12:26:04 +0300

    jsdoc for pull

--- src/util/pull.js
+++ src/util/pull.js
@@ -1,11 +1,17 @@
 const { exec } = require('child_process')
 
-const pull = () =>
+/**
+ *
+ * @param {boolean} [printStatus=true] Optional. If you want to hide logs pass false
+ * @param {Console} [logger=console] Optional. You can pass your own logger
+ * @returns {Promise<void>}
+ */
+const pull = (printStatus = true, logger = console) =>
   new Promise((resolve, reject) => {
     exec('git pull', (error, stdout, strerr) => {
       if (error) return reject(error)
-      if (stdout) console.log(stdout)
-      if (strerr) console.error(strerr)
+      if (stdout && printStatus) logger.log(stdout)
+      if (strerr && printStatus) logger.error(strerr)
       resolve()
     })
   })
